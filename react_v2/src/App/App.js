3d6420c58268e7e8f128c018d807a12ade62e396
Bohdan <bogdan.kashuba@gamenet.ru>
2020-03-20 23:14:17 +0300

    Added delete action on trash button

--- react_v2/src/App/App.js
+++ react_v2/src/App/App.js
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import AppHeader from '../components/app-header';
 import SearchPanel from'../components/search-panel';
 import TodoList from '../components/todo-list';
 import ItemStatusFilter from '../components/item-status-filter';
 
-const App = () => {
-  const todoData = [
-    {label: 'Drink Coffee', important: false, id: 'dc'},
-    {label: 'Make awesome app', important: true, id: 'map'},
-    {label: 'Have a lucnh', important: false, id: 'hal'}
-  ];
+export default class App extends Component {
 
-  return (
-    <div className="App">
-       <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+  state = {
+    todoData: [
+      {label: 'Drink Coffee', important: false, id: 'dc'},
+      {label: 'Make awesome app', important: true, id: 'map'},
+      {label: 'Have a lucnh', important: false, id: 'hal'}
+    ]
+  };
+
+  deletItem = (id) => {
+    this.setState(({todoData}) => {
+      const idx = todoData.findIndex((el) => el.id === id);
+      const newArray = [
+        ...todoData.slice(0, idx),
+        ...todoData.slice(idx + 1)
+      ];
+
+      return {
+        todoData: newArray
+      };
+    });
+  };
+
+  render(){
+
+    return (
+      <div className="App">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+
+        <TodoList todos={this.state.todoData}
+                  onDeleted={this.deletItem} />
       </div>
-
-      <TodoList todos={todoData} />
-    </div>
-  );
+    );
+  };
 }
 
-export default App;
+
